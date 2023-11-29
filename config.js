bbe00c2e3bde1a6c8aca55ee74e8a1d9eed7edc2
zarakikenpac <152364970+zarakikenpac@users.noreply.github.com>
2023-11-29 07:06:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', 'FAT', true],
+  ['6281255369012', 'MOMMY', true], 
   [''] 
 ] //Number of owners
 
